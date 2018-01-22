93e8741f8c39fe4d5ece30fe1abf7621cb7462f2
pr1nnark <robot@christiandresler.dk>
2018-01-22 22:45:23 +0100

    Think I got it now

--- recursion.js
+++ recursion.js
@@ -1,1 +1,23 @@
-// eleq js 3.2 init test file
+// eleq js 3.2a - non-recursive
+
+function isEven (x) {
+    if (x % 2 == 0)
+        return true;
+    else
+        return false;
+}
+
+
+// eleq js 3.2b - recursive
+
+function isEven (n) {
+    n = Math.abs(n);
+    if (n == 0)
+        return true;
+    else if (n == 1)
+        return false;
+    else
+        return isEven(n - 2);
+}
+
+// yak ak
