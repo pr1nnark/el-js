a21d72ec805e6baf8c3da5c88d141f30ea6923d5
pr1nnark <robot@christiandresler.dk>
2018-01-28 04:32:27 +0100

    ranger.js and 4.1 solution

---
+++ ranger.js
@@ -0,0 +1,19 @@
+// eleq js 4.1
+/* Write a range function that takes two arguments, start and end, and returns an array containing all the numbers from start up to (and including) end. */
+
+function ranger(x,y) {
+rangeArray = [];
+    for (var steps = x; y >= steps; steps++) {
+        rangeArray.push(steps);
+    }
+return rangeArray;
+}
+console.log(ranger(1, 10));
+
+
+// eleq js 4.1
+/* Next, write a sum function that takes an array of numbers and returns the sum of these numbers. */
+
+function sum() {
+    do loop array.length; while loop?
+}
